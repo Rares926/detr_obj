2a39fb5d9c692d473a78586c4c92232e99947344
Rares Patrascu <rares.patrascu@countthings.com>
2022-08-22 15:37:39 +0300

    Allow training with a custom dataset.

--- datasets/custom.py
+++ datasets/custom.py
@@ -1,9 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-COCO dataset which returns image_id for evaluation.
 
-Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
-"""
 from pathlib import Path
 
 import torch
@@ -14,14 +9,14 @@ from pycocotools import mask as coco_mask
 import datasets.transforms as T
 
 
-class CocoDetection(torchvision.datasets.CocoDetection):
+class CustomDetection(torchvision.datasets.CocoDetection):
     def __init__(self, img_folder, ann_file, transforms, return_masks):
-        super(CocoDetection, self).__init__(img_folder, ann_file)
+        super(CustomDetection, self).__init__(img_folder, ann_file)
         self._transforms = transforms
         self.prepare = ConvertCocoPolysToMask(return_masks)
 
     def __getitem__(self, idx):
-        img, target = super(CocoDetection, self).__getitem__(idx)
+        img, target = super(CustomDetection, self).__getitem__(idx)
         image_id = self.ids[idx]
         target = {'image_id': image_id, 'annotations': target}
         img, target = self.prepare(img, target)
@@ -146,13 +141,13 @@ def make_coco_transforms(image_set):
 
 def build(image_set, args):
     root = Path(args.data_path)
-    assert root.exists(), f'provided COCO path {root} does not exist'
+    assert root.exists(), f'provided dataset path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "images", root / "annotations" / f'{mode}_train.json'),
+        "val": (root / "images", root / "annotations" / f'{mode}_test.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
-    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    dataset = CustomDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
     return dataset
